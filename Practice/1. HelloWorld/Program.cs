9019e6e075d19b654ff22dfe408633601b254d8d
Filip Janješić <filip.janjesic@gmail.com>
2025-09-26 21:02:29 +0200

    Adding practice

---
+++ Practice/1. HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
---
+++ Practice/2. AddingNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number!");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number!");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"The sum of {a} and {b} is {a + b}");
+    }
+}
+// This program prompts the user to enter two numbers, reads the input, converts it to integers, and then calculates and displays their sum.
