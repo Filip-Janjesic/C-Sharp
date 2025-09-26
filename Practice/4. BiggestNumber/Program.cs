722ec645ad7fdbb275c0927278311923781d6048
Filip Janješić <filip.janjesic@gmail.com>
2025-09-26 21:42:11 +0200

    biggest number

---
+++ Practice/4. BiggestNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the third number: ");
+        int c = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the fourth number: ");
+        int d = int.Parse(Console.ReadLine());
+
+        int biggest = Math.Max(a , Math.Max(b, Math.Max(c, d)));
+        Console.WriteLine($"The biggest number is: {biggest}");
+    }
+}
