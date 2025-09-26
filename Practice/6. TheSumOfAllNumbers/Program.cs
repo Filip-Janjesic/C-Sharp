30595a153ef8d427fa345247dbcc60e703d5e3b7
Filip Janješić <filip.janjesic@gmail.com>
2025-09-26 21:42:58 +0200

    The sum of all numbers

---
+++ Practice/6. TheSumOfAllNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number n: ");
+        int n = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine($"The sum of 1 to {n} is: {sum}");
+    }
+}
