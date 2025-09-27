49c3ef47e2d8a2b3e4dd47f7ffe7de80b7d74487
Filip Janješić <filip.janjesic@gmail.com>
2025-09-27 11:27:40 +0200

    Revers string

---
+++ Practice/7. ReverseSring/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the word: ");
+        string word = Console.ReadLine();
+        char[] charArray = word.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedWord = new string(charArray);
+
+        Console.WriteLine("Reversed word: " + reversedWord);
+    }
+}
