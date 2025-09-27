db741dbbd0d4c1d4f4fc43b029463b5b4b7bb3b0
Filip Janješić <filip.janjesic@gmail.com>
2025-09-27 14:11:31 +0200

    Add 12. FibonacciSequence project

---
+++ Practice/12. FibonacciSequence/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci numbers up to " + n + ":");
+
+        int a = 0, b = 1;
+        while (a <= n)
+        {
+            Console.Write(a + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
