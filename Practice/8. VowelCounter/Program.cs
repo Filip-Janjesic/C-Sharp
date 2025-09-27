6ce12794bba80af492c541b68f30d472fcb46514
Filip Janješić <filip.janjesic@gmail.com>
2025-09-27 11:28:07 +0200

    vowel counter

---
+++ Practice/8. VowelCounter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+    {
+    static void Main()
+    {
+        Console.WriteLine("Enter the word: ");
+        string word = Console.ReadLine().ToLower();
+        int count = 0;
+
+        foreach (char c in word)
+        
+            if ("aeiou".Contains(c))
+            {
+                count++;
+            }
+
+            Console.WriteLine($"Number of vowels: {count}");
+        
+    }
+}
