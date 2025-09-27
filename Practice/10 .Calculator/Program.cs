9e4c87da0994ce97a04a1af978d669aeded2f44b
Filip Janješić <filip.janjesic@gmail.com>
2025-09-27 11:49:45 +0200

    calculator

---
+++ Practice/10 .Calculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number: ");
+        double a = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number: ");
+        double b = double.Parse(Console.ReadLine());
+
+        Console.Write("Choose an operation (+, -, *, /): ");
+        string op = Console.ReadLine();
+
+        double result = 0;
+
+        switch (op)
+        {
+            case "+":
+                result = a + b;
+                break;
+            case "-":
+                result = a - b;
+                break;
+            case "*":
+                result = a * b;
+                break;
+            case "/":
+                if (b != 0)
+                    result = a / b;
+                else
+                {
+                    Console.WriteLine("Error: Division by zero.");
+                    return;
+                }
+                break;
+            default:
+                Console.WriteLine("Error: Invalid operation.");
+                return;
+        }
+        Console.WriteLine($"Result: {result}");
+    }
+}
+// This is a simple calculator program that performs basic arithmetic operations.
---
+++ Practice/9. GuessTheNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random rand = new ();
+        int secretNumber = rand.Next(1, 101);
+        int attempts;
+
+        Console.WriteLine("Guess the number between 1 and 100!");
+
+        do
+        {
+            Console.Write("Enter your guess: ");
+            attempts = int.Parse(Console.ReadLine());
+
+            if (attempts < secretNumber)
+            {
+                Console.WriteLine("Too low! Try again.");
+            }
+            else if (attempts > secretNumber)
+            {
+                Console.WriteLine("Too high! Try again.");
+            }
+            else
+            {
+                Console.WriteLine("Congratulations! You've guessed the number!");
+            }
+        } while (attempts != secretNumber);
+    }
+}
