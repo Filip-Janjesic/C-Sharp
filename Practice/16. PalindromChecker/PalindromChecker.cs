ff5c5c171edbae86666d49bf7bbc7539041c2293
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 14:22:15 +0200

    Add Palindrome Checker example

---
+++ Practice/16. PalindromChecker/PalindromChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class PalindromeChecker
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a word: ");
+        string input = Console.ReadLine();
+        string reversed = new string(input.ToCharArray().Reverse().ToArray());
+
+        if (input.Equals(reversed, StringComparison.OrdinalIgnoreCase))
+        {
+            Console.WriteLine($"{input} is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine($"{input} is not a palindrome.");
+        }
+    }
+}
+// This code checks if a given word is a palindrome.
