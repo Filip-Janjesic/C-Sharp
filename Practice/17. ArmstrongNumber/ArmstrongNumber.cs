7765c5bae0466e398ac2f436359faa9e2c61ea57
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 17:00:25 +0200

    Clean up ignored files

---
+++ Practice/17. ArmstrongNumber/ArmstrongNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class ArmstrongNumber
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+        int sum = 0, temp = num;
+        int digits = num.ToString().Length;
+
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            sum += (int)Math.Pow(digit, digits);
+            temp /= 10;
+        }
+
+        if (sum == num)
+            Console.WriteLine($"{num} is an Armstrong number.");
+        else
+            Console.WriteLine($"{num} is not an Armstrong number.");
+    }
+}
+// This code checks if a number is an Armstrong number by calculating the sum of its digits raised to the power of the number of digits and comparing it to the original number.
---
+++ Practice/18. LeapYearChecker/LeapYearChecker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class LeapYearChecker
+{
+    static void Main()
+    {
+        Console.Write("Enter a year: ");
+        int year = int.Parse(Console.ReadLine());
+
+        if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+            Console.WriteLine($"{year} is a leap year.");
+        else
+            Console.WriteLine($"{year} is not a leap year.");
+    }
+}
+// This program checks if a given year is a leap year or not.
