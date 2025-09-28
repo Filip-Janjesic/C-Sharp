d42c74d2e7a15a206558ee7082475c718b7ac5ee
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 14:21:52 +0200

    Add Fibonacci Sequence example

---
+++ Practice/12. FibonacciSequence/FibonacciSequence.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class FibonacciSequence
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Fibonacci numbers up to " + n + ":");
+
+        int a = 0, b = 1;
+        while (a <= n)
+        {
+            Console.Write(a + " ");
+            int temp = a + b;
+            a = b;
+            b = temp;
+        }
+    }
+}
+// This program generates Fibonacci numbers up to a specified limit.
