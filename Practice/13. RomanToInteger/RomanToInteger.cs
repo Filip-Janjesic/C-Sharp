4bb8fc5ab2d4727057c0352ab64ff4a962f7ec24
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 14:21:55 +0200

    Add Roman to Integer example

---
+++ Practice/13. RomanToInteger/RomanToInteger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class RomanToInteger
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter roman number: ");
+        string roman = Console.ReadLine().ToUpper();
+
+        Console.WriteLine($"Integer value: {RomanToInteger(roman)}");
+    }
+    static int RomanToInteger(string s)
+    {
+        int total = 0;
+        int prevValue = 0;
+        foreach (char c in s)
+        {
+            int currentValue = c switch
+            {
+                'I' => 1,
+                'V' => 5,
+                'X' => 10,
+                'L' => 50,
+                'C' => 100,
+                'D' => 500,
+                'M' => 1000,
+                _ => throw new ArgumentException("Invalid Roman numeral character")
+            };
+            if (currentValue > prevValue)
+            {
+                total += currentValue - 2 * prevValue;
+            }
+            else
+            {
+                total += currentValue;
+            }
+            prevValue = currentValue;
+        }
+        return total;
+    }
+}
+// This code converts a Roman numeral string to its integer value.
