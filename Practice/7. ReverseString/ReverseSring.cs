508e5dbd2b5c08ea1a31357af61c27a4a423fc26
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 14:22:11 +0200

    Add Reverse String example

---
+++ Practice/7. ReverseString/ReverseSring.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ReverseSring
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the word: ");
+        string word = Console.ReadLine();
+        char[] charArray = word.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedWord = new string(charArray);
+
+        Console.WriteLine("Reversed word: " + reversedWord);
+    }
+}
+// This program prompts the user to enter a word, reads the input, reverses the characters in the word, and then displays the reversed word.
