31bdd2446f6bd7e577780456475c13c6432602c0
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 14:22:03 +0200

    Add Even or Odd Number example

---
+++ Practice/3. EvenOrOddNumber/EvenOrOddNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class EvenOrOddNumber
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0)
+        {
+            Console.WriteLine("The number is even.");
+        }
+        else
+        {
+            Console.WriteLine("The number is odd.");
+        }
+    }
+}
+// This program prompts the user to enter a number, reads the input, converts it to an integer, and then checks if the number is even or odd by using the modulus operator. It then displays the result accordingly.
