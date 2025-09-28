d578376d9b266ec4245b017c845b184e4848f1b3
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 14:22:05 +0200

    Add Biggest Number example

---
+++ Practice/4. BiggestNumber/BiggestNumber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class BiggestNumber
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number: ");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the third number: ");
+        int c = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the fourth number: ");
+        int d = int.Parse(Console.ReadLine());
+
+        int biggest = Math.Max(a , Math.Max(b, Math.Max(c, d)));
+        Console.WriteLine($"The biggest number is: {biggest}");
+    }
+}
+// This program prompts the user to enter four numbers, reads the input, converts it to integers, and then determines and displays the largest of the four numbers using the Math.Max method.
