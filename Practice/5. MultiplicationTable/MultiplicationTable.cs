6131c68c52429905a9ca936c51352736cd7fd449
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 14:22:07 +0200

    Add Multiplication Table example

---
+++ Practice/5. MultiplicationTable/MultiplicationTable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class MultiplicationTable
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine($"{n} x {i} = {n * i}");
+        }
+    }
+}
+// This program prompts the user to enter a number, reads the input, converts it to an integer, and then prints the multiplication table for that number from 1 to 10 using a for loop.
