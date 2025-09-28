072f71793852b797f6b7e493e0605fbe89744949
Filip Janješić <filip.janjesic@gmail.com>
2025-09-28 14:21:46 +0200

    Add HelloWorld example

---
+++ Practice/1. HelloWorld/HelloWorld.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class HelloWorld
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        string name = Console.ReadLine();
+        Console.WriteLine($"Hello, {name}!");
+    }
+}
+// This program prints "Hello, World!" to the console, then reads a name input from the user and greets them with "Hello, [name]!".
