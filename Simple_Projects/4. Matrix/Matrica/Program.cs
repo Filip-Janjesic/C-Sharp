274a237b8705375fd798467e49dc37bf5ff98123
Filip Janješić <filip.janjesic@gmail.com>
2025-11-20 21:57:28 +0100

    Matrica based on what i did with PHP

---
+++ Simple_Projects/4. Matrix/Matrica/Program.cs
@@ -0,0 +1,28 @@
+﻿using Matrica;
+
+Matrix m1 = new Matrix(2, 2, Matrix.Filltype.Random);
+Matrix m2 = new Matrix(2, 2, Matrix.Filltype.Random);
+Matrix m3 = new Matrix(2, 3, Matrix.Filltype.Random);
+
+Console.WriteLine(m1);
+Console.WriteLine(m2);
+Console.WriteLine(m3);
+
+m1[0, 0] = 10;
+
+Console.WriteLine(m1.GetValue(0, 0));
+Console.WriteLine(m2++);
+
+
+//Console.WriteLine(m2!=m1);
+//Console.WriteLine(m1 <= m2);
+
+//try
+//{
+//    m1.Add(m3);
+//    Console.WriteLine(m1);
+//}
+//catch (Exception e)
+//{
+//    Console.WriteLine(e.Message);
+//}
